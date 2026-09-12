31ecb8b33d23338b46da2b5db8a7a3445e3f301a
Adtz47 <aditya7980kumar@gmail.com>
2026-09-12 23:40:39 +0530

    laytest

--- triply-ai-master/src/app/_layout.tsx
+++ triply-ai-master/src/app/_layout.tsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+// this is the publishable key for your Clerk frontend API, which is safe to expose to the public. It is used to initialize the ClerkProvider and enable authentication features in your app.
 
 if (!publishableKey) {
   throw new Error("Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY. Add it to your .env file.");
